d19a3388fd8d6944b56e3881699abb575b6cfa17
github username <github email address>
2017-12-14 23:44:09 -0800

    more form building

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,2 +1,27 @@
 class InvitesController < ApplicationController
+  before_action :set_list, only: [:show, :new, :create, :edit, :update, :destroy]
+
+
+  def create
+    @invite = @list.invites.new(invite_params)
+
+    if @invite.save
+      flash[:notice] = "Success"
+      redirect_to edit_list_path(@list.id)
+    else
+      flash[:notice] = "Oh no!"
+      redirect_to edit_list_path(@list.id)
+    end
+  end
+
+   private 
+    
+    def invite_params
+      params.require(:invite).permit(:email)
+    end
+
+    def set_list
+      @list = List.find(params[:list_id])
+    end
+
 end
