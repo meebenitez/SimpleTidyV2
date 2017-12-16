4825a860bad1137ab638550ae88da16df0ff8e0d
github username <github email address>
2017-12-15 23:41:42 -0800

    refactored some list index stuff

--- app/helpers/invites_helper.rb
+++ app/helpers/invites_helper.rb
@@ -9,19 +9,7 @@ module InvitesHelper
     end
   end
 
-  def list_type_photo(list_id)
-    if list = List.find_by(id: list_id)
-      if list.list_type == "home"
-        url = "#{house.png}"
-      elsif list.list_type == "car"
-        url = "#{car.png}"
-      else
-        url = "#{laptop.png}"
-      end
-    else
-      nil
-    end
-  end
+  
 
 
 end
--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,13 @@
 module ListsHelper
+
+
+  def list_type_photo(list)
+      if list.list_type == "Home"
+        url = "house.png"
+      elsif list.list_type == "Car"
+        url = "car.png"
+      else
+        url = "laptop.png"
+      end
+  end
 end
