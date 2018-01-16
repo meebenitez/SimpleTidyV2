bbc94d850d9f959062ed05a2e39a01615d932e09
github username <github email address>
2018-01-16 14:40:23 -0800

    updated createChore model object to have id

--- app/serializers/chore_serializer.rb
+++ app/serializers/chore_serializer.rb
@@ -1,4 +1,4 @@
 class ChoreSerializer < ActiveModel::Serializer
-  attributes :name, :frequency, :time_of_day, :status, :past_due
+  attributes :id, :name, :frequency, :time_of_day, :status, :past_due
   belongs_to :list
 end
