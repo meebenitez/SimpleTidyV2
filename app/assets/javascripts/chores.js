0e657de903dec5e28ef2a1aafeeafdb93e43b519
github username <github email address>
2018-02-14 21:29:46 -0800

    created function to alphabetically sort to make more DRY

--- app/assets/javascripts/chores.js
+++ app/assets/javascripts/chores.js
@@ -18,22 +18,6 @@ function formatName(chore){
     }
 }
 
-Chore.prototype.formatEditListing = function() {
-    //debugger;  
-  return `
-            <li class="${this.frequency}" id="<%= chore.id%>">${this.name} <a href="#" class="delete-chore" data-id='[${this.id}, ${this.listID}]'>✖</a><a href="/lists/${this.listID}/chores/${this.id}">✎</a></li>
-
-            `
-}
-
-function formatEditListing (chore, listID) {
-    //debugger;  
-  return `
-            <li class="${chore.frequency}" id="<%= chore.id%>">${chore.name} <a href="#" class="delete-chore" data-id='[${chore.id}, ${listID}]'>✖</a><a href="/lists/${listID}/chores/${chore.id}">✎</a></li>
-
-            `
-}
-
 Chore.prototype.formatCompletedButton = function() {
     return `
             <div class="completed-item">
@@ -44,8 +28,111 @@ Chore.prototype.formatCompletedButton = function() {
 
             `
 }
+/////////////////////////HELPER FUNCITONS
 
+function sortChoresABC(chores, data){
+    var sorted = chores.sort(function(a,b){
+        if (a.name.toLowerCase() < b.name.toLowerCase()){
+            return -1
+        }
+        if (a.name.toLowerCase() > b.name.toLowerCase()){
+            return 1
+        }
+        return 0
+    })
+    $(`#${data.frequency} > li`).remove()
+    sorted.forEach(function(chore){
+        var choreHTML = formatEditListing(chore, data.list.id)
+            $(`#${chore.frequency}`).append(choreHTML);
+    })
+}
 
+function formatEditListing (chore, listID) {
+  return `
+            <li class="${chore.frequency}" id="<%= chore.id%>">${chore.name} <a href="#" class="delete-chore" data-id='[${chore.id}, ${listID}]'>✖</a><a href="/lists/${listID}/chores/${chore.id}">✎</a></li>
+
+            `
+}
+
+  
+//////////////////////////CHORE CREATION
+
+$(function () {
+    $(".create-chore").removeAttr("data-disable-with")
+    //$(".create-chore").click(function(){
+        $('#new_chore').submit(function(event) {
+            event.preventDefault();
+            var action = $(this).attr('action')
+            var values = $(this).serialize();
+            var posting = $.post(action, values);
+            
+            posting.done(function(data) {
+              $.get(`/lists/${data.list.id}`, function(response){
+                  //grab all chores with the same frequency as the newly created chore
+                var chores = response.chores.filter(chore => chore.frequency === data.frequency)
+                if (chores.length > 1) {
+                    sortChoresABC(chores, data)
+                } else {
+                    $(`#${data.frequency}`).empty()
+                    var choreHTML = formatEditListing(data, data.list.id)
+                        $(`#${data.frequency}`).append(choreHTML);
+                }
+              }) 
+            });
+    });
+});
+
+//////////////////////////CHORE DELETION
+
+$(function () {
+    $(document).on('click','.delete-chore', function(event) {
+        var ids = $(this).data("id");
+        var url = `/lists/${ids[1]}/chores/${ids[0]}`
+        $.ajax ({
+            method: 'DELETE',
+            url: url,
+            headers: {
+        'X-Transaction': 'POST Example',
+        'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+        }, 
+        }).done(function(data){
+            if ($(`.${data.frequency}`).length === 1) {
+            $(`#${data.id}`).remove();
+            $(`#${data.frequency}`).html(`<br><br><center>You need to create some ${data.frequency} chores.</center><br>`)
+            } else {
+                $.get(`/lists/${data.list.id}`, function(response){
+                    var chores = response.chores.filter(chore => chore.frequency === data.frequency)
+                    sortChoresABC(chores, data)
+                })
+            }
+        })
+        event.preventDefault();
+    })
+})
+  
+
+////setup radio buttons
+function resetradio () {
+    var radio = document.getElementsByName('chore[frequency]')[0];
+    var buttons = document.querySelector('.buttons');
+    var radios = document.getElementsByName('chore[time_of_day]');
+    radios[2].checked = true;
+    if (radio.checked == true) {
+        buttons.style.display = 'block';
+    }
+    else {
+        buttons.style.display = 'none';
+        radios.forEach(function(radio){
+          radio.checked = false;
+        })
+    }
+}
+function setradio () {
+    var radio = document.getElementsByName('chore[frequency]')[0];
+    if (radio.checked == false) {
+        radio.checked = true;
+    }
+}
 
 
 //////////////////////////CHORE COMPLETION
@@ -78,99 +165,4 @@ $(function () {
   });
 
 
-  });
-
-  
-//////////////////////////CHORE CREATION
-
-
-$(function () {
-    $(".create-chore").removeAttr("data-disable-with")
-    //$(".create-chore").click(function(){
-        $('#new_chore').submit(function(event) {
-            event.preventDefault();
-            var action = $(this).attr('action')
-            var values = $(this).serialize();
-            var posting = $.post(action, values);
-            
-            posting.done(function(data) {
-              $.get(`/lists/${data.list.id}`, function(response){
-                  //grab all chores with the same frequency as the newly created chore
-                var chores = response.chores.filter(chore => chore.frequency === data.frequency)
-                if (chores.length > 1) {
-                    var sorted = chores.sort(function(a,b){
-                        //sort chores alphabetically
-                    if (a.name.toLowerCase() < b.name.toLowerCase()){
-                        return -1
-                    }
-                    if (a.name.toLowerCase() > b.name.toLowerCase()){
-                        return 1
-                    }
-                    return 0
-                  })
-                  $(`#${data.frequency} > li`).remove()
-                  sorted.forEach(function(chore){
-                    var choreHTML = formatEditListing(chore, data.list.id)
-                        $(`#${chore.frequency}`).append(choreHTML);
-                  })
-                } else {
-                    $(`#${data.frequency}`).empty()
-                    var choreHTML = formatEditListing(data, data.list.id)
-                        $(`#${data.frequency}`).append(choreHTML);
-                }
-              }) 
-            });
-    });
 });
-  
-
-////setup radio buttons
-function resetradio () {
-    var radio = document.getElementsByName('chore[frequency]')[0];
-    var buttons = document.querySelector('.buttons');
-    var radios = document.getElementsByName('chore[time_of_day]');
-    radios[2].checked = true;
-    if (radio.checked == true) {
-        buttons.style.display = 'block';
-    }
-    else {
-        buttons.style.display = 'none';
-        radios.forEach(function(radio){
-          radio.checked = false;
-        })
-    }
-}
-function setradio () {
-    var radio = document.getElementsByName('chore[frequency]')[0];
-    if (radio.checked == false) {
-        radio.checked = true;
-    }
-}
-
-
-//////////////////////////CHORE DELETION
-
-$(function () {
-    $(document).on('click','.delete-chore', function(event) {
-        var ids = $(this).data("id");
-        var url = `/lists/${ids[1]}/chores/${ids[0]}`
-        $.ajax ({
-            method: 'DELETE',
-            url: url,
-            headers: {
-        'X-Transaction': 'POST Example',
-        'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
-        }, 
-        }).done(function(data){
-            if ($(`.${data.frequency}`).length === 1) {
-            $(`#${data.id}`).remove();
-            $(`#${data.frequency}`).html(`<br><br><center>You need to create some ${data.frequency} chores.</center><br>`)
-            } else {
-            //$(`#${data.id}`).remove(); // don't need this if I'm reloading the divs
-            $(`#${data.frequency}`).load(location.href+` #${data.frequency}>*`,"");
-            }
-        })
-        event.preventDefault();
-    })
-})
-
