4bce35d1dc3b09329c1151610cfaca3da2cebfa3
github username <meebenitez@gmail.com>
2018-08-12 23:28:00 -0700

    updated devise.rb for facebook api change

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -247,7 +247,7 @@ Devise.setup do |config|
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
 
-  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], token_params: { parse: :json }
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
